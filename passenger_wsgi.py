e73033c8c3592f3757761bbb4ea95690b57b22c9
Hendra Manudinata <manoedinata@gmail.com>
2024-09-04 20:59:26 +0700

    Minor changes, to avoid adjustment for DirectAdmin

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,10 @@
 from sendwa import create_app
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-app = create_app()
-app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+application = create_app()
+application.wsgi_app = ProxyFix(
+    application.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
