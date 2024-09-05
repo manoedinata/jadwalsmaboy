b1debd483181779b6c725996961d1041c77c735d
Hendra Manudinata <manoedinata@gmail.com>
2024-09-05 17:52:20 +0700

    Update greet

--- sendwa/routes.py
+++ sendwa/routes.py
@@ -41,7 +41,7 @@ def siswa_add():
     greetingTexts += "Notifikasi jadwal akan dikirimkan ke kamu melalui nomor ini. "
     greetingTexts += "Silahkan di-save jika mau. \n"
     greetingTexts += "\n"
-    greetingTexts += "Layanan akan dimulai secepatnya nanti sore. \n"
+    greetingTexts += "Layanan akan dimulai secepatnya. \n"
     greetingTexts += "\n"
     greetingTexts += f"Nama: *{nama}* \n"
     if panggilan: greetingTexts += f"Panggilan kustom: *{panggilan}* \n"
